9ae319348d30aff18e3a8e0d8889ddad132b042d
warnercp15 <warnercp15@gmail.com>
2021-03-22 12:51:28 -0600

    Solved first PR

--- gift-box-shop-frontend/pages/Shop.js
+++ gift-box-shop-frontend/pages/Shop.js
@@ -1,30 +1,30 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import PageWrapper from '../src/components/PageWrapper'
-import ShopMenu from '../src/components/ShopMenu';
+import ShopMenu from '../src/components/ShopMenu'
 
 export default function Shop() {
+    const [stepsList, setStepsList] = useState([])
+    const [currentStep, setCurrentStep] = useState('1')
 
-    const [stepsList,setStepsList]=useState([]);  
-
-    useEffect(()=>{
-
+    useEffect(() => {
         //Fetch/Axios Request API
-        setStepsList(
-            [
-                {title:'1. Box Type',check:false},
-                {title:'2. Pastry',check:false},
-                {title:'3. Skin Products',check:true},
-                {title:'4. Extras',check:false},
-                {title:'5. Review',check:false}
-            ]
-        );
-
-    },[])
+        setStepsList([
+            { id: '1', title: 'Box Type' },
+            { id: '2', title: 'Pastry' },
+            { id: '3', title: 'Skin Products' },
+            { id: '4', title: 'Extras' },
+            { id: '5', title: 'Review' },
+        ])
+    }, [])
 
     return (
         <PageWrapper>
-            <ShopMenu stepsList={stepsList}></ShopMenu>
+            <ShopMenu
+                stepsList={stepsList}
+                currentStep={currentStep}
+                setCurrentStep={setCurrentStep}
+            ></ShopMenu>
         </PageWrapper>
-    );
+    )
 }
--- gift-box-shop-frontend/src/components/ShopMenu.js
+++ gift-box-shop-frontend/src/components/ShopMenu.js
@@ -1,11 +1,23 @@
-import styles from '../../styles/components/ShopMenu.module.scss';
+import styles from '../../styles/components/ShopMenu.module.scss'
 
-export default function ShopMenu({stepsList}) {
+export default function ShopMenu({ stepsList, currentStep, setCurrentStep }) {
+    function handleClick(option) {
+        setCurrentStep(option)
+    }
+
     return (
         <div className={styles.containerShopMenu}>
-            {stepsList.map(step=>(
-                <p className={step.check? styles.selectedOption:''} key={step.title.split('.')[0]}>{step.title}</p> 
+            {stepsList.map((step) => (
+                <button
+                    className={step.id === currentStep ? styles.selectedOption : ''}
+                    key={step.id}
+                    onClick={() => {
+                        handleClick(step.id)
+                    }}
+                >
+                    {step.id}. {step.title}
+                </button>
             ))}
         </div>
-    );
+    )
 }
