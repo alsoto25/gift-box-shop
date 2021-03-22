cb5f3d28929fd20a204bfb1680376a9230f517dd
Alejandro Soto <alejandro.soto@wundermanthompson.com>
2021-03-22 14:03:45 -0600

    Merging changes in feature/BuildMyGiftManu branch

--- gift-box-shop-frontend/pages/Shop.js
+++ gift-box-shop-frontend/pages/Shop.js
@@ -1,6 +1,29 @@
-import Head from 'next/head'
+import React, { useState, useEffect } from 'react'
 import PageWrapper from '../src/components/global/PageWrapper'
+import ShopMenu from '../src/components/ShopMenu'
 
 export default function Shop() {
-    return <PageWrapper>Shop</PageWrapper>
+    const [stepsList, setStepsList] = useState([])
+    const [currentStep, setCurrentStep] = useState('1')
+
+    useEffect(() => {
+        //Fetch/Axios Request API
+        setStepsList([
+            { id: '1', title: 'Box Type' },
+            { id: '2', title: 'Pastry' },
+            { id: '3', title: 'Skin Products' },
+            { id: '4', title: 'Extras' },
+            { id: '5', title: 'Review' },
+        ])
+    }, [])
+
+    return (
+        <PageWrapper>
+            <ShopMenu
+                stepsList={stepsList}
+                currentStep={currentStep}
+                setCurrentStep={setCurrentStep}
+            ></ShopMenu>
+        </PageWrapper>
+    )
 }
