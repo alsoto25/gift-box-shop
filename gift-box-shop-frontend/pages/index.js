60adef0f2d5231f05058e3d64eaa40152711f7b1
fitoalfarooo <fitoalfa098@gmail.com>
2021-04-05 20:46:46 -0600

    SliderHome

--- gift-box-shop-frontend/pages/index.js
+++ gift-box-shop-frontend/pages/index.js
@@ -1,7 +1,48 @@
 import Head from 'next/head'
-import styles from '../styles/pages/Home.module.scss'
+import React, {useState, useEffect} from 'react';
 import PageWrapper from '../src/components/global/PageWrapper'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import styles from '../styles/pages/Home.module.scss'
+
+import { homeResponse } from '../test/homeResponse'
 
 export default function Home() {
-    return <PageWrapper>Home</PageWrapper>
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        //Fetch/Axios Request API
+        setTimeout(function () {
+            setImages(homeResponse);
+        }, 300)
+    }, [])
+
+   const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    fade: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    className: styles['slides'],
+    lazyLoad: true,
+    pauseOnHover: true,
+  };
+
+    return <PageWrapper>
+        <div>
+            <Slider {...settings}>
+                {images.map((img)=>{
+                    return(
+                        <img width="100%" height="460vh" src={img.url}/>
+                    )
+                })}
+            </Slider>
+        </div>
+    </PageWrapper>
 }
