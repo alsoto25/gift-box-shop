6ac2a3c5f84f1fdd3f8ce7fad6002c501747432a
warnercp15 <warnercp15@gmail.com>
2021-04-17 16:02:15 -0600

    base cms structure

---
+++ gift-box-cms-frontend/pages/Content.js
@@ -0,0 +1,6 @@
+import Head from 'next/head'
+import PageWrapper from '../src/components/global/PageWrapper'
+
+export default function Content() {
+    return <PageWrapper>Content</PageWrapper>
+}
---
+++ gift-box-cms-frontend/pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+import styles from '../styles/pages/Home.module.scss'
+import PageWrapper from '../src/components/global/PageWrapper'
+
+export default function Home() {
+    return <PageWrapper>Home</PageWrapper>
+}
