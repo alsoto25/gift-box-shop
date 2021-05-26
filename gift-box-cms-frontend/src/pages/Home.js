89c3c857e6740a17eb9b2fb89cd9c1d8a3e0f62a
Alejandro Soto <al.soto25@gmail.com>
2021-05-26 15:42:31 -0600

    [TECH-7.1] Added CMS Home

--- gift-box-cms-frontend/src/pages/Home.js
+++ gift-box-cms-frontend/src/pages/Home.js
@@ -8,133 +8,24 @@ import ShopSocials from '../components/home/ShopSocials'
 import styles from '../styles/pages/Home.module.scss'
 import axios from 'axios';
 
-const homeResponse = {
-    slides: [
-        {
-            img: {
-                url:
-                    'https://images.pexels.com/photos/2072148/pexels-photo-2072148.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                alt: 'Photo1',
-            },
-            text: 'A gift for <span>every</span> occasion',
-        },
-        {
-            img: {
-                url:
-                    'https://images.pexels.com/photos/2072181/pexels-photo-2072181.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                alt: 'Photo2',
-            },
-            text: '<span>Tailor</span> your gift to your taste',
-        },
-        {
-            img: {
-                url:
-                    'https://images.pexels.com/photos/205961/pexels-photo-205961.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                alt: 'Photo3',
-            },
-            text: 'More than <span>15 years</span> of experience',
-        },
-        {
-            img: {
-                url:
-                    'https://images.pexels.com/photos/264771/pexels-photo-264771.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                alt: 'Photo4',
-            },
-            text: 'Surprise your loved ones with <span>exactly</span> what they love',
-        },
-    ],
-    testimonials: {
-        title: 'Join other satisfied customers',
-        testimonialsList: [
-            {
-                id: 'vmt',
-                img: {
-                    url:
-                        'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-                    alt: 'Woman in collared shirt smiling',
-                },
-                name: 'Vanessa Morales',
-                testimonial:
-                    'I gifted my mother a box with everything she likes and she absolutely loved it!',
-            },
-            {
-                id: 'rcht',
-                img: {
-                    url:
-                        'https://images.pexels.com/photos/262391/pexels-photo-262391.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                    alt: 'Man wearing eyeglasses',
-                },
-                name: 'Roberto Chaves',
-                testimonial:
-                    "I'm not usually great with gifts, but bought one for my wife and she was absolutely stunned.",
-            },
-            {
-                id: 'jst',
-                img: {
-                    url:
-                        'https://images.pexels.com/photos/3290236/pexels-photo-3290236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                    alt: 'Woman wearing turtleneck white shirt smiling',
-                },
-                name: 'Johanna Stapleton',
-                testimonial:
-                    "I absolutely love the amount of customization the is! I gave one box to each ember of my family and they're all completely different from each other!",
-            },
-        ],
-    },
-    socials: {
-        shopCTA: {
-            text: 'Check out the shop',
-            url: '/Shop',
-        },
-        socials: [
-            {
-                id: 'fb',
-                url: 'https://www.facebook.com/repasa2011.giftbox.cr',
-                img: {
-                    url: '/img/social/facebook.png',
-                    alt: 'Facebook logo',
-                },
-            },
-            {
-                id: 'ig',
-                url: 'https://www.instagram.com/reposteriarepasa/',
-                img: {
-                    url: '/img/social/instagram.png',
-                    alt: 'Instagram logo',
-                },
-            },
-            {
-                id: 'wa',
-                url: 'tel:506-8787-2695',
-                img: {
-                    url: '/img/social/whatsapp.png',
-                    alt: 'Whatsapp logo',
-                },
-            },
-        ],
-    },
-}
-
-
 export default function Home() {
     const [content, setContent] = useState(null)
 
     useEffect(() => {
-        setContent(homeResponse)
         //Fetch/Axios Request API
-        // axios.get('http://localhost:3001/home/getHomeInfo')
-        //     .then(res => {
-        //         console.log('RES',res);
-        //         if(res.request.statusText==="OK"){
-        //             setContent(res.data.homeResponse)
-        //         }
-        //         else if(res.request.statusText==="INTERNAL_SERVER_ERROR"){
-        //             console.log('ERROR',res)
-        //         }
-        //     })
-        //     .catch(err => {
-        //         console.log(err)
-        //     })
+        axios.get('http://localhost:3001/home/getHomeInfo')
+            .then(res => {
+                console.log('RES',res);
+                if(res.request.statusText==="OK"){
+                    setContent(res.data.homeResponse)
+                }
+                else if(res.request.statusText==="INTERNAL_SERVER_ERROR"){
+                    console.log('ERROR',res)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [])
 
     const [currentFileName] = useState('')
